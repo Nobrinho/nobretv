b0df7b339476a7b8f59c504ff7a625145a2e42f4
Nobrinho <emerson_14@live.com>
2024-09-07 12:14:23 -0400

    feat: improvements mobile home

--- src/components/ContainerCard.vue
+++ src/components/ContainerCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="p-10 grid grid-cols-4 gap-4 sm:grid-cols-4 sm:gap-4 md:gap-8 lg:gap-12 xl:gap-12">
+    <div class=" px-2 grid grid-cols-3 gap-4 sm:grid-cols-4 sm:gap-2 md:gap-8 lg:gap-12 xl:gap-12">
         <slot></slot>
     </div>
 </template>
